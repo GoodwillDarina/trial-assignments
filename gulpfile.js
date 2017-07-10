4725400dac4af9456828dc58b4ee2168f625ffd1
Daria Saburova <saburova@azoft.com>
2017-07-10 22:23:29 +0700

    settings gulpfile and markup pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    del = require('del'),
-    plumber = require('gulp-plumber'),
-    browserSync = require("browser-sync"),
-    reload = browserSync.reload;
+const gulp = require('gulp'),
+      less = require('gulp-less'),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      source = require('vinyl-source-stream'),
+      buffer = require('vinyl-buffer'),
+      del = require('del'),
+      rigger = require('gulp-rigger'),
+      plumber = require('gulp-plumber'),
+      browserSync = require('browser-sync'),
+      reload = browserSync.reload;
 
 gulp.task('clean:css', cleanCss);
 gulp.task('clean', clean);
@@ -46,7 +47,7 @@ function cleanCss() {
 }
 
 function compileCss() {
-  return gulp.src('./less/main.less')
+  return gulp.src('./source/less/main.less')
       .pipe(less({ strictMath: true }))
       .pipe(postcss([
         autoprefixer({ browsers: ['> 1%', 'IE 9', 'IE 10']})
@@ -56,17 +57,20 @@ function compileCss() {
 
 function copyIndex() {
   return gulp.src('./source/index.html')
+      .pipe(rigger())
       .pipe(gulp.dest('./app'))
       .pipe(reload({stream: true}));
 }
 
 function copyImages() {
   return gulp.src('./source/images/**/*.*')
+      .pipe(plumber())
       .pipe(gulp.dest('./app/images'));
 }
 
 function copyFonts() {
   return gulp.src('./source/fonts/**/*.*')
+      .pipe(plumber())
       .pipe(gulp.dest('./app/fonts'));
 }
 
